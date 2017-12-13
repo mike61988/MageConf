beff39f7dcfbd565ddec9f75a321b477ab4235c8
mike61988 <micmolandroid@gmail.com>
2017-12-13 14:34:33 +0200

    step5

---
+++ FrontendGridUi/view/frontend/requirejs-config.js
@@ -0,0 +1,15 @@
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+var config = {
+
+    config: {
+        mixins: {
+            'Magento_Ui/js/grid/columns/column': {
+                'MageConf_FrontendGridUi/js/render/grid-mixin': true
+            }
+        }
+    }
+};
